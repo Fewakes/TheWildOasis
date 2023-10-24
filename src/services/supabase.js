0e698e9f6e2d22b17e195f3c0df202359f0170b4
Fewakes <Slojkowski33@gmail.com>
2023-10-25 00:24:24 +0100

    3rd Commit
    Setting up the backend (Supabase) & Buckets along with RLS. Setting up React Query, Setting up Toasts (Library), Setting up React Hook Form (Library) Connecting the App to the backend api.

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://mqhujrncetqgtaamdkyq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xaHVqcm5jZXRxZ3RhYW1ka3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwMjQ1MTYsImV4cCI6MjAxMzYwMDUxNn0.3L0F-52YNuxMoM30YeOcw1g4AtMeuNCsQ-K_BH9CO3Q";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
